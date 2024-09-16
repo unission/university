dfe82bee1ed1ec0894387e148e62930e2d414e4d
Chenvel <105103515+Pussia@users.noreply.github.com>
2024-09-16 20:19:06 +0600

    Default Architecture

---
+++ src/main/java/org/unission/university/University2Application.java
@@ -0,0 +1,13 @@
+package org.unission.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class University2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(University2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/unission/university/University2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unission.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class University2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
